77d1782613aca7278719492c5b57d87dda9c4cfb
Alexandra <a.galitzine@gmail.com>
2020-05-03 11:38:23 +0100

    refactored code

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 function main() {
 
-  // Start screen set up 
+  // Imported items from index.html
   const start = document.querySelector('.start')
+  const gameboard = document.querySelector('.gameboard')
   const startButton = document.querySelector('.play')
   const audio = document.querySelector('#audio')
+  const gameover = document.querySelector('.gameover')
+  const restartButton = document.querySelector('.restart')
+  const livesText = document.querySelector('.lives')
+  const pointsText = document.querySelector('.points')
 
+  // Start screen set up 
   startButton.addEventListener('click', () => {
+    gameboard.style.display = 'flex'
     start.style.display = 'none'
     audio.src = 'music/Super Mario Bros. medley.mp3' 
     audio.play()
@@ -17,47 +24,41 @@ function main() {
   const grid = document.querySelector('.grid')
   const cells = []
 
-  // Imported items
-  const gameover = document.querySelector('.gameover')
-  const restartButton = document.querySelector('.restart')
-  const livesText = document.querySelector('.lives')
-  const pointsText = document.querySelector('.points')
-
   let lives = 3
   let points = 0
   let functionCalled = 0
 
   // Items on the grid
-  const pcIndex = [2, 4, 6]
-  const gameboyIndex = [54, 57, 60]
-  const netflixIndex = [53, 50, 47]
-  const catIndex = [27, 30, 33]
-  const drinkIndex = [26, 23, 20]
+  const homeIndex = [2, 4, 6]
+  const DKIndex = [54, 57, 60]
+  const marioIndex = [53, 50, 47]
+  const mushroomIndex = [27, 30, 33]
+  const finnIndex = [26, 23, 20]
 
-  let lili = 76
+  let princess = 76
   let intervalTime = 1000
 
-  // Construction of the grid and placing items in cell locations at the beginning of the game
+  // Construction of the grid and placing items in cell lomushroomions at the beginning of the game
   for (let i = 0; i < gridCellCount; i++) {
     const cell = document.createElement('div')
     cell.classList.add('cell')
-    if (i === lili) {
-      cell.classList.add('lili')
+    if (i === princess) {
+      cell.classList.add('princess')
     }
-    if (i === pcIndex[0] || i === pcIndex[1] || i === pcIndex[2]) {
-      cell.classList.add('pc')
+    if (i === homeIndex[0] || i === homeIndex[1] || i === homeIndex[2]) {
+      cell.classList.add('home')
     }
-    if (i === gameboyIndex[0] || i === gameboyIndex[1] || i === gameboyIndex[2]) {
-      cell.classList.add('gameboy')
+    if (i === DKIndex[0] || i === DKIndex[1] || i === DKIndex[2]) {
+      cell.classList.add('DK')
     }
-    if (i === netflixIndex[0] || i === netflixIndex[1] || i === netflixIndex[2]) {
-      cell.classList.add('netflix')
+    if (i === marioIndex[0] || i === marioIndex[1] || i === marioIndex[2]) {
+      cell.classList.add('mario')
     }
-    if (i === catIndex[0] || i === catIndex[1] || i === catIndex[2]) {
-      cell.classList.add('cat')
+    if (i === mushroomIndex[0] || i === mushroomIndex[1] || i === mushroomIndex[2]) {
+      cell.classList.add('mushroom')
     }
-    if (i === drinkIndex[0] || i === drinkIndex[1] || i === drinkIndex[2]) {
-      cell.classList.add('drink')
+    if (i === finnIndex[0] || i === finnIndex[1] || i === finnIndex[2]) {
+      cell.classList.add('finn')
     }
     grid.appendChild(cell)
     cells.push(cell)
@@ -66,164 +67,165 @@ function main() {
   // Movement for the key character
   document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowRight') {
-      if (lili === cells.length - 1) {
+      if (princess === cells.length - 1) {
         return
       }
-      cells[lili].classList.remove('lili')
-      lili += 1
-      cells[lili].classList.add('lili')
+      cells[princess].classList.remove('princess')
+      princess += 1
+      cells[princess].classList.add('princess')
     } else if (event.key === 'ArrowLeft') {
-      if (lili === 0) {
+      if (princess === 0) {
         return
       }
-      cells[lili].classList.remove('lili')
-      lili -= 1
-      cells[lili].classList.add('lili')
+      cells[princess].classList.remove('princess')
+      princess -= 1
+      cells[princess].classList.add('princess')
     } else if (event.key === 'ArrowUp') {
-      if (lili < width) {
+      if (princess < width) {
         return
       }
-      cells[lili].classList.remove('lili')
-      lili -= width
-      cells[lili].classList.add('lili')
+      cells[princess].classList.remove('princess')
+      princess -= width
+      cells[princess].classList.add('princess')
     } else if (event.key === 'ArrowDown') {
-      if (lili > cells.length - width - 1) {
+      if (princess > cells.length - width - 1) {
         return
       }
-      cells[lili].classList.remove('lili')
-      lili += width
-      cells[lili].classList.add('lili')
+      cells[princess].classList.remove('princess')
+      princess += width
+      cells[princess].classList.add('princess')
     }
-    if (cells[lili].classList.contains('gameboy') || cells[lili].classList.contains('netflix') || cells[lili].classList.contains('cat') || cells[lili].classList.contains('drink')) {
-      cells[lili].classList.remove('lili')
-      lili = 76
-      cells[76].classList.add('lili')
+    if (cells[princess].classList.contains('DK') || cells[princess].classList.contains('mario') || cells[princess].classList.contains('mushroom') || cells[princess].classList.contains('finn')) {
+      cells[princess].classList.remove('princess')
+      princess = 76
+      cells[76].classList.add('princess')
       lives -= 1
       livesText.innerHTML = `${lives}`
       gameOver()
     }
-    if (cells[lili].classList.contains('pc')) {
+    if (cells[princess].classList.contains('home')) {
       getHome()
     }
   })
 
   // Loops for the moving obstacles
-  const gameboyInterval = function () {
+  const DKInterval = function () {
     setInterval(() => {
-      for (let i = 0; i < gameboyIndex.length; i++) {
-        if (gameboyIndex[i] > 53 && gameboyIndex[i] <= 62) {
-          if (gameboyIndex[i] === 62) {
-            cells[62].classList.remove('gameboy')
-            gameboyIndex[i] -= (width)
-            cells[gameboyIndex[i]].classList.add('gameboy')
+      for (let i = 0; i < DKIndex.length; i++) {
+        if (DKIndex[i] > 53 && DKIndex[i] <= 62) {
+          if (DKIndex[i] === 62) {
+            cells[62].classList.remove('DK')
+            DKIndex[i] -= (width)
+            cells[DKIndex[i]].classList.add('DK')
           } else {
-            if (cells[gameboyIndex[i] + 1].classList.contains('lili')) {
-              cells[gameboyIndex[i] + 1].classList.remove('lili')
-              lili = 76
-              cells[76].classList.add('lili')
+            if (cells[DKIndex[i] + 1].classList.contains('princess')) {
+              cells[DKIndex[i] + 1].classList.remove('princess')
+              princess = 76
+              cells[76].classList.add('princess')
               lives -= 1
               livesText.innerHTML = `${lives}`
               gameOver()
             }
           }
-          cells[gameboyIndex[i]].classList.add('gameboy')
-          cells[gameboyIndex[i]].classList.remove('gameboy')
-          gameboyIndex[i] += 1
-          cells[gameboyIndex[i]].classList.add('gameboy')
+          cells[DKIndex[i]].classList.add('DK')
+          cells[DKIndex[i]].classList.remove('DK')
+          DKIndex[i] += 1
+          cells[DKIndex[i]].classList.add('DK')
         }
       }
     }, intervalTime)
   }
 
-  gameboyInterval()
+  DKInterval()
 
-  const netflixInterval = function () {
+  const marioInterval = function () {
     setInterval(() => {
-      for (let i = 0; i < netflixIndex.length; i++) {
-        if (netflixIndex[i] >= 45 && netflixIndex[i] < 54) {
-          if (netflixIndex[i] === 45) {
-            cells[45].classList.remove('netflix')
-            netflixIndex[i] += (width)
-            cells[netflixIndex[i]].classList.add('netflix')
+      for (let i = 0; i < marioIndex.length; i++) {
+        if (marioIndex[i] >= 45 && marioIndex[i] < 54) {
+          if (marioIndex[i] === 45) {
+            cells[45].classList.remove('mario')
+            marioIndex[i] += (width)
+            cells[marioIndex[i]].classList.add('mario')
           } else {
-            if (cells[netflixIndex[i] - 1].classList.contains('lili')) {
-              cells[netflixIndex[i] - 1].classList.remove('lili')
-              lili = 76
-              cells[76].classList.add('lili')
+            if (cells[marioIndex[i] - 1].classList.contains('princess')) {
+              cells[marioIndex[i] - 1].classList.remove('princess')
+              princess = 76
+              cells[76].classList.add('princess')
               lives -= 1
               livesText.innerHTML = `${lives}`
               gameOver
             }
           }
-          cells[netflixIndex[i]].classList.remove('netflix')
-          netflixIndex[i] -= 1
-          cells[netflixIndex[i]].classList.add('netflix')
+          cells[marioIndex[i]].classList.remove('mario')
+          marioIndex[i] -= 1
+          cells[marioIndex[i]].classList.add('mario')
         }
       }
     }, intervalTime)
   }
 
-  netflixInterval()
+  marioInterval()
 
-  const catInterval = function () {
+  const mushroomInterval = function () {
     setInterval(() => {
-      for (let i = 0; i < catIndex.length; i++) {
-        if (catIndex[i] > 26 && catIndex[i] <= 35) {
-          if (catIndex[i] === 35) {
-            cells[35].classList.remove('cat')
-            catIndex[i] -= (width)
-            cells[catIndex[i]].classList.add('cat')
+      for (let i = 0; i < mushroomIndex.length; i++) {
+        if (mushroomIndex[i] > 26 && mushroomIndex[i] <= 35) {
+          if (mushroomIndex[i] === 35) {
+            cells[35].classList.remove('mushroom')
+            mushroomIndex[i] -= (width)
+            cells[mushroomIndex[i]].classList.add('mushroom')
           } else {
-            if (cells[catIndex[i] + 1].classList.contains('lili')) {
-              cells[catIndex[i] + 1].classList.remove('lili')
-              lili = 76
-              cells[76].classList.add('lili')
+            if (cells[mushroomIndex[i] + 1].classList.contains('princess')) {
+              cells[mushroomIndex[i] + 1].classList.remove('princess')
+              princess = 76
+              cells[76].classList.add('princess')
               lives -= 1
               livesText.innerHTML = `${lives}`
               gameOver()
             }
           }
-          cells[catIndex[i]].classList.add('cat')
-          cells[catIndex[i]].classList.remove('cat')
-          catIndex[i] += 1
-          cells[catIndex[i]].classList.add('cat')
+          cells[mushroomIndex[i]].classList.add('mushroom')
+          cells[mushroomIndex[i]].classList.remove('mushroom')
+          mushroomIndex[i] += 1
+          cells[mushroomIndex[i]].classList.add('mushroom')
         }
       }
     }, intervalTime)
   }
 
-  catInterval()
+  mushroomInterval()
 
-  const drinkInterval = function () {
+  const finnInterval = function () {
     setInterval(() => {
-      for (let i = 0; i < drinkIndex.length; i++) {
-        if (drinkIndex[i] >= 18 && drinkIndex[i] < 27) {
-          if (drinkIndex[i] === 18) {
-            cells[18].classList.remove('drink')
-            drinkIndex[i] += (width)
-            cells[drinkIndex[i]].classList.add('drink')
+      for (let i = 0; i < finnIndex.length; i++) {
+        if (finnIndex[i] >= 18 && finnIndex[i] < 27) {
+          if (finnIndex[i] === 18) {
+            cells[18].classList.remove('finn')
+            finnIndex[i] += (width)
+            cells[finnIndex[i]].classList.add('finn')
           } else {
-            if (cells[drinkIndex[i] - 1].classList.contains('lili')) {
-              cells[drinkIndex[i] - 1].classList.remove('lili')
-              lili = 76
-              cells[76].classList.add('lili')
+            if (cells[finnIndex[i] - 1].classList.contains('princess')) {
+              cells[finnIndex[i] - 1].classList.remove('princess')
+              princess = 76
+              cells[76].classList.add('princess')
               lives -= 1
               livesText.innerHTML = `${lives}`
               gameOver()
             }
           }
-          cells[drinkIndex[i]].classList.remove('drink')
-          drinkIndex[i] -= 1
-          cells[drinkIndex[i]].classList.add('drink')
+          cells[finnIndex[i]].classList.remove('finn')
+          finnIndex[i] -= 1
+          cells[finnIndex[i]].classList.add('finn')
         }
       }
     }, intervalTime)
   }
 
-  drinkInterval()
+  finnInterval()
 
   function gameOver() {
     if (lives === 0) {
+      gameboard.style.display = 'none'
       gameover.style.display = 'block'
       restartButton.addEventListener('click', () => {
         window.location.reload()
@@ -232,11 +234,11 @@ function main() {
   }
 
   function getHome() {
-    cells[lili].classList.remove('pc')
-    cells[lili].classList.remove('lili')
-    cells[lili].classList.add('success')
-    lili = 76
-    cells[76].classList.add('lili')
+    cells[princess].classList.remove('home')
+    cells[princess].classList.remove('princess')
+    cells[princess].classList.add('success')
+    princess = 76
+    cells[76].classList.add('princess')
     points += 100
     pointsText.innerHTML = `${points}`
     functionCalled += 1
@@ -245,19 +247,19 @@ function main() {
   const functionCalledInterval = setInterval(() => {
     if (functionCalled !== 0 && functionCalled === 3) {
       clearInterval(functionCalledInterval)
-      clearInterval(gameboyInterval)
-      clearInterval(netflixInterval)
-      clearInterval(drinkInterval)
-      clearInterval(catInterval)
+      clearInterval(DKInterval)
+      clearInterval(marioInterval)
+      clearInterval(finnInterval)
+      clearInterval(mushroomInterval)
       alert('Good work! Now lets try going a little faster')
       intervalTime /= 2
-      gameboyInterval()
-      netflixInterval()
-      drinkInterval()
-      catInterval()
-      for (let i = 0; i < pcIndex.length; i++) {
-        cells[pcIndex[i]].classList.remove('success')
-        cells[pcIndex[i]].classList.add('pc')
+      DKInterval()
+      marioInterval()
+      finnInterval()
+      mushroomInterval()
+      for (let i = 0; i < homeIndex.length; i++) {
+        cells[homeIndex[i]].classList.remove('success')
+        cells[homeIndex[i]].classList.add('home')
       }
     }
   }, 200)
